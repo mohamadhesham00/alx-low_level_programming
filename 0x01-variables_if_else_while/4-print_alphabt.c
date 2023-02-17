b6b36bd06f0b61ba5a5a0ea5ed0befd8bed70b00
mohamadhesham00 <mohamadhesham112@gmail.com>
2023-02-17 14:09:32 -0800

    day 1 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	int i;
+
+	for (i = 1; i <= 26; i++){
+		if (c != 'q'&& c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
