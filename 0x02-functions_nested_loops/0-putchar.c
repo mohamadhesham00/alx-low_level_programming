ec77bd3c02af6aba0ee5942dd5a8a9ac39d785bc
mohamadhesham00 <mohamadhesham112@gmail.com>
2023-02-21 10:03:14 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point for printing a sentence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word="_putchar;
+	
+	int i;
+	
+	for (i = 0; i < word.size; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/RRADME.md
@@ -0,0 +1,2 @@
+this is for day 2 in learning c language 
+and it'ts about functions and loops
