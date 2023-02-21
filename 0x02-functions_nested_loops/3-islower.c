5c6ef087d51216f002b46fdf0c4075dcc3331aff
mohamadhesham00 <mohamadhesham112@gmail.com>
2023-02-21 11:08:11 -0800

    3 and 4 added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check the code for determing whether it's lower or upper case
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
