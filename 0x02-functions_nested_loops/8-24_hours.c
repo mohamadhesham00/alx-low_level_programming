a7be2e6f54c6ff8109aa28f7be3136b46a3b1716
mohamadhesham00 <mohamadhesham112@gmail.com>
2023-02-21 18:45:01 -0800

    8 edited

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,30 @@ void jack_bauer(void)
 
 	for (x = 0; x <= 23; x++)
 	{
-		for (y = 0; y <=59; y++)
+		for (y = 0; y <= 59; y++)
 		{
 			if (x <= 9)
+			{
 				_putchar('0');
-			_putchar('0'+x);
+				_putchar('0' + x);
+			}
+			else
+			{
+				_putchar(x / 10 + '0');
+				_putchar(x % 10 + '0');
+			}
 			_putchar(':');
 			if (y <= 9)
+			{
 				_putchar('0');
-			_putchar('0'+y);
+				_putchar('0' + y);
+			}
+			else
+			{
+				_putchar(y / 10 + '0');
+				_putchar(y % 10 + '0');
+			}
+			_putchar('\n');
 		}
 	}
 }			       	
