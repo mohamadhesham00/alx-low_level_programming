1ced149b453cd21a614801fe71bfa63a6a62ce0e
mohamadhesham00 <mohamadhesham112@gmail.com>
2023-02-21 18:15:35 -0800

    until 9 added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (y = 0; y <=59; y++)
+		{
+			if (x <= 9)
+				_putchar('0');
+			_putchar('0'+x);
+			_putchar(':');
+			if (y <= 9)
+				_putchar('0');
+			_putchar('0'+y);
+		}
+	}
+}			       	
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{	
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	for (i = 1; i <= 81; i++)
+	{
+		_putchar('0'+i);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		if (i % 9  == 0)
+		{
+		_putchar('\n');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		}
+	}
+}
